e8ba1544f83b4fa6819d44f7ee262f66161ccb9d
Javier Ortiz <javier.ortiz@coverwallet.com>
2020-11-25 14:55:35 +0100

    Declare constants inside function

--- taxes/get-tax.js
+++ taxes/get-tax.js
@@ -1,13 +1,15 @@
 const taxRegulator = require('./tax-regulators')
 const calculator = require('./calculator')
 
-const cityTaxes = {
-    "NY": {vat: 23, specific: {city: 1, insurance: 2}, state: 5},
-    "AL": {vat: 30, specific: { cold_weather: 5 }, state: 0},
-};
-const defaultTax = { vat: 21, state: 2}
+
 
 const targetTax = (state) => {
+    const cityTaxes = {
+        "NY": {vat: 23, specific: {city: 1, insurance: 2}, state: 5},
+        "AL": {vat: 30, specific: { cold_weather: 5 }, state: 0},
+    };
+    const defaultTax = { vat: 21, state: 2}
+
     return (cityTaxes[state] || defaultTax)
 }
 
@@ -20,9 +22,9 @@ module.exports = (ctx) => {
     if (!newCtx.request.body.state || !newCtx.request.body.productPrice) {
         return { error: "Required parameters not present" };
     }
-    const taxes = targetTax(newCtx.request.body.state);
 
     try {
+        const taxes = targetTax(newCtx.request.body.state);
         const resultCalculator = calculator(newCtx.request.body.productPrice, taxes);
 
         if (newCtx.request.body.notifyRegulator) {
